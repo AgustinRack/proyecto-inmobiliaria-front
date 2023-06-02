1c8fbe7be3392dfc46aa9011df1ef4397468a516
Gonzalo Molina <molinagomezgonzalo@gmail.com>
2023-06-02 15:33:11 -0300

    nav and login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-function NavScrollExample() {
+function NavigationBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="/">HOD.</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -17,21 +17,19 @@ function NavScrollExample() {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="/users">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
+            <Nav.Link href="/users">En venta</Nav.Link>
+            <Nav.Link href="#action2">Alquiler</Nav.Link>
+            <Nav.Link href="/users">Agenda tu visita</Nav.Link>
+            <NavDropdown title="más" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/login">login</NavDropdown.Item>
+              <NavDropdown.Item href="/signup">signup</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">
-                Something else here
+                Nuestros servicios
               </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Nosotros</NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Contacto</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -48,66 +46,4 @@ function NavScrollExample() {
   );
 }
 
-export default NavScrollExample;
-
-// import React from "react";
-// import { Link, useLocation } from "react-router-dom";
-
-// function Navbar() {
-//   const location = useLocation();
-//   const isLoggedIn = true;
-
-//   let navbarContent;
-
-//   if (isLoggedIn) {
-//     navbarContent = (
-//       <ul className="navbar-nav">
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/venta">
-//             En Venta
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/alquiler">
-//             En Alquiler
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/visits">
-//             Agenda tu Visita
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/servicios">
-//             Nuestros Servicios
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/perfil">
-//             Mi Perfil
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/nosotros">
-//             Nosotros
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link className="nav-link" to="/contact">
-//             Contacto
-//           </Link>
-//         </li>
-//       </ul>
-//     );
-//   } else {
-//     navbarContent = <span>HOUSE OF DEV</span>;
-//   }
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <div className="container">{navbarContent}</div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
+export default NavigationBar;
