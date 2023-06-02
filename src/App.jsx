4e646b7aa6e7f4bb58388e05504ca3c507e1dca5
Gonzalo Molina <molinagomezgonzalo@gmail.com>
2023-06-02 12:17:16 -0300

    fixed settings route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as settings from "./settings";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
@@ -6,12 +7,13 @@ import { useDispatch } from "react-redux";
 import Navbar from "./components/Navbar";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SingUp";
+import { loginSuccess } from "./state/user/userSlice";
 
 function App() {
   useEffect(() => {
     async function fetchUser() {
       const user = await axios.get(`${settings.axiosURL}/users/me`);
-      await dispatch(login(user.data));
+      await dispatch(loginSuccess(user.data));
     }
     fetchUser();
   }, []);
