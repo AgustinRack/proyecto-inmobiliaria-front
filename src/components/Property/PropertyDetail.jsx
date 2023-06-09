fb2c880e2472fdd5f353bea46bdbb8119678472d
Gonzalo Molina <molinagomezgonzalo@gmail.com>
2023-06-09 15:58:13 -0300

    images styles

--- src/components/Property/PropertyDetail.jsx
+++ src/components/Property/PropertyDetail.jsx
@@ -13,11 +13,16 @@ export const PropertyDetail = () => {
   return (
     <div>
       <h2>Description:</h2>
-      <p>{description}</p>
+      <p style={{ fontSize: "1.6em" }}>{description}</p>
       <h2>Images:</h2>
       <div>
         {imgs.map((img, index) => (
-          <img key={index} src={img} alt={`Image ${index}`} />
+          <img
+            key={index}
+            src={img}
+            alt={`Image ${index}`}
+            style={{ width: "500px", height: "auto", margin: "10px" }}
+          />
         ))}
       </div>
     </div>
